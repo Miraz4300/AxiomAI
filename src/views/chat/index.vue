af0ba0154bfdca81748bc0024a6f96f1030d3b2b
Miraz Hossain <miraz4300@gmail.com>
2023-03-19 18:29:03 +0000

    preview-build

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -476,8 +476,8 @@ onUnmounted(() => {
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <!-- AxiomAI is being introduced. -->
               <div class="text-gray-800 w-full md:max-w-2xl lg:max-w-3xl md:h-full md:flex md:flex-col px-6 dark:text-gray-100">
-                <h1 class="text-4xl font-semibold mt-[20vh] ml-auto mr-auto mb-16">
-                  AxiomAI
+                <h1 class="text-4xl font-semibold mt-[18vh] ml-auto mr-auto mb-16">
+                  AxiomAI-preview
                 </h1><div class="flex items-start text-center gap-3.5">
                   <div class="flex flex-col gap-3.5 flex-1">
                     <svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6 m-auto" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="5" /><line x1="12" y1="1" x2="12" y2="3" /><line x1="12" y1="21" x2="12" y2="23" /><line x1="4.22" y1="4.22" x2="5.64" y2="5.64" /><line x1="18.36" y1="18.36" x2="19.78" y2="19.78" /><line x1="1" y1="12" x2="3" y2="12" /><line x1="21" y1="12" x2="23" y2="12" /><line x1="4.22" y1="19.78" x2="5.64" y2="18.36" /><line x1="18.36" y1="5.64" x2="19.78" y2="4.22" /></svg><h2 class="text-lg font-normal">
