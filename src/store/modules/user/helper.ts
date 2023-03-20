5228f040530727ca4e8f02a92152e41632356897
Miraz Hossain <miraz4300@gmail.com>
2023-03-20 16:33:50 +0000

    fixed avatar url and modified some formal typo

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,7 +15,7 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/miraz4300/AxiomAI/main/src/assets/avatar.jpg',
+      avatar: 'https://raw.githubusercontent.com/miraz4300/AxiomAI/master/src/assets/avatar.jpg',
       name: 'Miraz Hossain',
       description: 'Give a star on <a href="https://github.com/miraz4300/AxiomAI" class="text-blue-500" target="_blank" >GitHub</a>',
     },
