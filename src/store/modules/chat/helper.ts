5c968ddc6d188734f9a5274cda0820fbbb95344d
Miraz Hossain <miraz4300@gmail.com>
2023-03-21 12:02:31 +0000

    Follow-up mode

--- src/store/modules/chat/helper.ts
+++ src/store/modules/chat/helper.ts
@@ -6,7 +6,7 @@ export function defaultState(): Chat.ChatState {
   const uuid = 1002
   return {
     active: uuid,
-    usingContext: true,
+    usingContext: false, // Follow-up Mode. Follow-up Mode is a feature that allows you to continue the conversation with the same context. This will carry the previous chat records. | Default: false
     history: [{ uuid, title: 'New Chat', isEdit: false }],
     chat: [{ uuid, data: [] }],
   }
