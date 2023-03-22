1f9965d907b6ac5a2f730f902abe89d5a0853e43
Miraz Hossain <miraz4300@gmail.com>
2023-03-22 12:56:19 +0000

    automatically redirect to home page

--- src/views/exception/500/index.vue
+++ src/views/exception/500/index.vue
@@ -15,10 +15,10 @@ function goHome() {
     <div class="px-4 m-auto space-y-4 text-center max-[400px]">
       <header class="space-y-2">
         <h2 class="text-2xl font-bold text-center text-slate-800 dark:text-neutral-200">
-          Error Code: 500 Internal Server Error
+          500 Internal Server Error
         </h2>
         <p class="text-base text-center text-slate-500 dark:text-slate-500">
-          The server encountered an unexpected condition that prevented it from fulfilling the request. Don't worry, we're working on it.
+          Don't worry, you will be automatically redirected when everything comes online.
         </p>
         <div class="flex items-center justify-center text-center">
           <Icon500 class="w-[300px]" />
