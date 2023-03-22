aa76ab7faf011cf961a13c4077dc39f20e7d4b10
Miraz Hossain <miraz4300@gmail.com>
2023-03-22 13:32:32 +0000

    typo fixed

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ function setupPlugins(env: ImportMetaEnv): PluginOption[] {
         short_name: 'AxiomAI',
         icons: [
           { src: 'axiomai-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'axiomai-192x192.png', sizes: '512x512', type: 'image/png' },
+          { src: 'axiomai-512x512.png', sizes: '512x512', type: 'image/png' },
         ],
       },
     })
