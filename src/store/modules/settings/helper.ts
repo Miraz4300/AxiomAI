20ebc35f4145fe725f5c07b41b8210e8b58206a0
Miraz Hossain <miraz4300@gmail.com>
2023-03-31 15:03:31 +0000

    update system message

--- src/store/modules/settings/helper.ts
+++ src/store/modules/settings/helper.ts
@@ -7,9 +7,8 @@ export interface SettingsState {
 }
 
 export function defaultSetting(): SettingsState {
-  const currentDate = new Date().toISOString().split('T')[0]
   return {
-    systemMessage: `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Current date: ${currentDate}`,
+    systemMessage: 'You are ChatGPT, a large language model trained by OpenAI. Follow the user\'s instructions carefully. Respond using markdown.',
   }
 }
 
