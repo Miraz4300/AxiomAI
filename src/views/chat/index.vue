dd67dfefcb3754044eb3665815402d3a90f31e6c
Miraz Hossain <miraz4300@gmail.com>
2023-04-04 10:32:06 +0000

    more responsive introduction and code optimization

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -496,98 +496,43 @@ catch (error) {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <!-- AxiomAI is being introduced. -->
-              <div
-                class="text-gray-800 w-full md:max-w-2xl lg:max-w-3xl md:h-full md:flex md:flex-col px-6 dark:text-gray-100"
-              >
-                <h1 class="text-4xl font-semibold mt-[18vh] ml-auto mr-auto mb-16">
+              <div class="text-gray-800 w-full md:max-w-2xl lg:max-w-3xl md:h-full md:flex md:flex-col px-6 dark:text-gray-100">
+                <h1 class="text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center">
                   AxiomAI-preview
-                </h1>
-                <div class="flex items-start text-center gap-3.5">
-                  <div class="flex flex-col gap-3.5 flex-1">
-                    <svg
-                      stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round"
-                      stroke-linejoin="round" class="w-6 h-6 m-auto" height="1em" width="1em"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle cx="12" cy="12" r="5" />
-                      <line x1="12" y1="1" x2="12" y2="3" />
-                      <line x1="12" y1="21" x2="12" y2="23" />
-                      <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
-                      <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
-                      <line x1="1" y1="12" x2="3" y2="12" />
-                      <line x1="21" y1="12" x2="23" y2="12" />
-                      <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
-                      <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
-                    </svg>
-                    <h2 class="text-lg font-normal">
-                      Examples
-                    </h2>
-                    <ul class="flex flex-col gap-3.5">
-                      <li
-                        class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md after:content-['&quot;'] before:content-['&quot;']"
-                      >
-                        Explain quantum computing in simple terms.
-                      </li>
-                      <li
-                        class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md after:content-['&quot;'] before:content-['&quot;']"
-                      >
-                        Got any creative ideas for a 10 year old's birthday?
-                      </li>
-                      <li
-                        class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md after:content-['&quot;'] before:content-['&quot;']"
-                      >
-                        How do I make an HTTP request in Javascript?
-                      </li>
+                </h1><div class="md:flex items-start text-center gap-3.5">
+                  <div class="flex flex-col mb-8 md:mb-auto gap-3.5 flex-1">
+                    <h2 class="flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2">
+                      <svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="5" /><line x1="12" y1="1" x2="12" y2="3" /><line x1="12" y1="21" x2="12" y2="23" /><line x1="4.22" y1="4.22" x2="5.64" y2="5.64" /><line x1="18.36" y1="18.36" x2="19.78" y2="19.78" /><line x1="1" y1="12" x2="3" y2="12" /><line x1="21" y1="12" x2="23" y2="12" /><line x1="4.22" y1="19.78" x2="5.64" y2="18.36" /><line x1="18.36" y1="5.64" x2="19.78" y2="4.22" /></svg>Examples
+                    </h2><ul class="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
+                      <button class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-emerald-700 dark:hover:bg-emerald-700">
+                        "Explain quantum computing in simple terms" →
+                      </button><button class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-emerald-700 dark:hover:bg-emerald-700">
+                        "Got any creative ideas for a 10 year old’s birthday?" →
+                      </button><button class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-emerald-700 dark:hover:bg-emerald-700">
+                        "How do I make an HTTP request in Javascript?" →
+                      </button>
                     </ul>
-                  </div>
-                  <div class="flex flex-col gap-3.5 flex-1">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                      stroke="currentColor" aria-hidden="true" class="w-6 h-6 m-auto"
-                    >
-                      <path
-                        stroke-linecap="round" stroke-linejoin="round"
-                        d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
-                      />
-                    </svg>
-                    <h2 class="text-lg font-normal">
-                      Capabilities
-                    </h2>
-                    <ul class="flex flex-col gap-3.5">
+                  </div><div class="flex flex-col mb-8 md:mb-auto gap-3.5 flex-1">
+                    <h2 class="flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-6 w-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" /></svg>Capabilities
+                    </h2><ul class="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
                       <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
                         Remembers what user said earlier in the conversation
-                      </li>
-                      <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
+                      </li><li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
                         Allows user to provide follow-up corrections
-                      </li>
-                      <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
+                      </li><li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
                         Trained to decline inappropriate requests
                       </li>
                     </ul>
-                  </div>
-                  <div class="flex flex-col gap-3.5 flex-1">
-                    <svg
-                      stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round"
-                      stroke-linejoin="round" class="w-6 h-6 m-auto" height="1em" width="1em"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"
-                      />
-                      <line x1="12" y1="9" x2="12" y2="13" />
-                      <line x1="12" y1="17" x2="12.01" y2="17" />
-                    </svg>
-                    <h2 class="text-lg font-normal">
-                      Limitations
-                    </h2>
-                    <ul class="flex flex-col gap-3.5">
+                  </div><div class="flex flex-col mb-8 md:mb-auto gap-3.5 flex-1">
+                    <h2 class="flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2">
+                      <svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z" /><line x1="12" y1="9" x2="12" y2="13" /><line x1="12" y1="17" x2="12.01" y2="17" /></svg>Limitations
+                    </h2><ul class="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
                       <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
                         May occasionally generate incorrect information
-                      </li>
-                      <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
+                      </li><li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
                         May occasionally produce harmful instructions or biased content
-                      </li>
-                      <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
+                      </li><li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
                         Limited knowledge of world and events after 2021
                       </li>
                     </ul>
